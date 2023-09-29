59c0acfb381d5da35a772b183c5bb1c58874ef0e
GoldsolacePVG <118179149+GoldsolacePVG@users.noreply.github.com>
2023-09-29 13:39:26 +0200

    Nergigante script complete

--- Assets/Code/Classes/Character/Monsters/Nergigante.cs
+++ Assets/Code/Classes/Character/Monsters/Nergigante.cs
@@ -1,2 +1,11 @@
 public class Nergigante : Monster {
+  public Nergigante() {
+    name = "Nergigante";
+    id = 7;
+    attack = 500;
+    defence = 600;
+    health = 5000;
+    race = "Elder Dragon";
+    element = (int)Elements.kRaw;
+  }
 }
