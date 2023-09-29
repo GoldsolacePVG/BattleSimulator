b343d4e3c9bd5c7bdb16e4f3cb3d53490bab0f3d
GoldsolacePVG <118179149+GoldsolacePVG@users.noreply.github.com>
2023-09-29 13:32:30 +0200

    Glavenus script complete

--- Assets/Code/Classes/Character/Monsters/Glavenus.cs
+++ Assets/Code/Classes/Character/Monsters/Glavenus.cs
@@ -2,5 +2,10 @@ public class Glavenus : Monster {
   public Glavenus() {
     name = "Glavenus";
     id = 6;
+    attack = 200;
+    defence = 400;
+    health = 3000;
+    race = "Brute Wyvern";
+    element = (int)Elements.kFire;
   }
 }
